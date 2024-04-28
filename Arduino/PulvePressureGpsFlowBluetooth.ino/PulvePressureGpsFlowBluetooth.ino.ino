2744d82951cd3d798c9b610470e9ad55678f9b0c
arhturarhtur <github@lucea.fr>
2024-04-28 16:30:08 +0200

    Update PulvePressureGpsFlowBluetooth.ino.ino

--- Arduino/PulvePressureGpsFlowBluetooth.ino/PulvePressureGpsFlowBluetooth.ino.ino
+++ Arduino/PulvePressureGpsFlowBluetooth.ino/PulvePressureGpsFlowBluetooth.ino.ino
@@ -18,6 +18,9 @@ TinyGPSPlus gps;
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 1000;
+unsigned long startMillisPressure;
+unsigned long currentMillisPressure;
+const unsigned long periodPressure = 20;
 String  bluetoothMsg = "";  //Message sent through Bluetooth
 
 //#define USE_PIN // Uncomment this to use PIN during pairing. The pin is specified on the line below
@@ -28,8 +31,8 @@ String device_name = "PulvePressure";
 int calib = 415;
 
 // Flowmeter
-const byte debitmetre1=32; // broche utilisée pour déclencher l'interruption du debitmètre 1
-const byte debitmetre2=33; // broche utilisée pour déclencher l'interruption du debitmètre 1
+const byte debitmetre1 = 32; // broche utilisée pour déclencher l'interruption du debitmètre 1
+const byte debitmetre2 = 33; // broche utilisée pour déclencher l'interruption du debitmètre 1
 volatile long nbPulse1 = 0;  // compteur d'impulsions débitmètre 1
 volatile long nbPulse2 = 0;  // compteur d'impulsions débitmètre 2
 byte pulses1;
@@ -39,13 +42,8 @@ long NombreDimpulsions2 = 0;
 float debit1;
 float debit2;
 unsigned long timer;
-const int NbImpulsionsDebitmetre = 1000;  //Flowmeter pulses
-void ICACHE_RAM_ATTR comptage1() {
-  nbPulse1++;
-}
-void ICACHE_RAM_ATTR comptage2() {
-  nbPulse2++;
-}
+const int NbImpulsionsDebitmetre1 = 980;  //Flowmeter pulses
+const int NbImpulsionsDebitmetre2 = 980;  //Flowmeter pulses
 
 const int n = 20; // Nombre de valeurs à prendre en compte
 int mesures[n];   // Tableau pour stocker les mesures
@@ -65,6 +63,13 @@ float pressure, voltage, sat, lon, llat, sspeed;
 
 BluetoothSerial SerialBT;
 
+void ICACHE_RAM_ATTR comptage1() {  // incremente nbPulse1 à chaque interruption
+  nbPulse1++;
+}
+void ICACHE_RAM_ATTR comptage2() {  // incremente nbPulse1 à chaque interruption
+  nbPulse2++;
+}
+
 void setup() {
   Serial.begin(115200);
   neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
@@ -80,8 +85,8 @@ void setup() {
   pinMode(debitmetre1, INPUT_PULLUP);
   pinMode(debitmetre2, INPUT_PULLUP);
   // Mettre la broche du débimetre en interrupt, assignation de la fonction comptage et réglage en FALLING (NPN)
-  attachInterrupt(digitalPinToInterrupt(debitmetre1), comptage1, FALLING);
-  attachInterrupt(digitalPinToInterrupt(debitmetre2), comptage2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(debitmetre1), comptage1, FALLING);  // a chaque interruption lance comptage1
+  attachInterrupt(digitalPinToInterrupt(debitmetre2), comptage2, FALLING);  // a chaque interruption lance comptage2
 }
 
 void loop() {
@@ -95,46 +100,59 @@ if(currentMillis - startMillis >= period) // Calcule et envoie toutes les 1 seco
     nbPulse1 = 0;
     pulses2 = nbPulse2;
     nbPulse2 = 0;
-    timer = millis();
     // Réactive les interruptions
     interrupts();
         
     // 1000 pulse = 1L
     // L/min
-    debit1 = 60000.0 / (timer - startMillis) * pulses1 / NbImpulsionsDebitmetre;
-    debit2 = 60000.0 / (timer - startMillis) * pulses2 / NbImpulsionsDebitmetre;
+    debit1 = 60000.0 / (currentMillis - startMillis) * pulses1 / NbImpulsionsDebitmetre1/2;
+    debit2 = 60000.0 / (currentMillis - startMillis) * pulses2 / NbImpulsionsDebitmetre2/2;
 
-    // Remplissage du tableau
-    while (ind < n) {
-      mesures[ind] = analogRead(PIN_PRESSURE); // Lecture de la valeur du capteur
+    unsigned long tempsPasse = currentMillis - startMillis;
+    Serial.print("Temps passe : ");
+    Serial.println(tempsPasse);
+    Serial.print("pulses 1 : ");
+    Serial.println(pulses1);
+    Serial.print("Debit 1 : ");
+    Serial.println(debit1);
+    
+/*    currentMillisPressure = millis();
+    if(currentMillisPressure - startMillisPressure >= periodPressure) // 
+      {*/
+
+        // Remplissage du tableau pression
+        while (ind < n) {
+          mesures[ind] = analogRead(PIN_PRESSURE); // Lecture de la valeur du capteur
+
   
-  
-      // Calcul de la médiane et de la moyenne
-      int mediane = calculerMedian();
-      float moyenneFiltree = calculerMoyenneSansOutliers();
-    /*  
-      Serial.println("Valeurs du tableau :");
-      for (int i = 0; i < n; i++) {
-        Serial.print(mesures[i]);
-        Serial.print(" ");
+        // Calcul de la médiane et de la moyenne
+        int mediane = calculerMedian();
+        float moyenneFiltree = calculerMoyenneSansOutliers();
+      /*  
+        Serial.println("Valeurs du tableau :");
+        for (int i = 0; i < n; i++) {
+          Serial.print(mesures[i]);
+          Serial.print(" ");
+        }
+        Serial.println();*/
+        
+        // Affichage des résultats
+        Serial.print("Mediane: ");
+        Serial.println(mediane);
+        //Serial.print("Moyenne sans outliers: ");
+        //Serial.println(moyenneFiltree);
+        
+        pressure = ((mediane-calib)*2.400/4096.000)*4+1;
+        
+  //      Serial.print("Val :");
+  //      Serial.println(val);
+  //      Serial.print("pressure :");
+  //      Serial.println(pressure,3);
+    
+        ind++; // Incrémentez l'indice
       }
-      Serial.println();*/
-      
-      // Affichage des résultats
-      Serial.print("Mediane: ");
-      Serial.println(mediane);
-      //Serial.print("Moyenne sans outliers: ");
-      //Serial.println(moyenneFiltree);
-      
-      pressure = ((mediane-calib)*2.400/4096.000)*4+1;
-      
-//      Serial.print("Val :");
-//      Serial.println(val);
-//      Serial.print("pressure :");
-//      Serial.println(pressure,3);
-  
-      ind++; // Incrémentez l'indice
-    }
+/*      startMillisPressure = currentMillisPressure;
+    }*/
   
     // Réinitialisation pour la prochaine série de mesures
     ind = 0;
